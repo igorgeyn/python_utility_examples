5366e6e1808011e0a623f467bb80c584449d0d26
Igor Geyn <igorgeyn@gmail.com>
2022-06-01 10:23:06 -0700

    pushing python tide files for Danny

---
+++ danny.py
@@ -0,0 +1,12 @@
+
+
+### This is a script to access NOAA tide and current data
+
+import pandas as pd
+import numpy as np
+
+### you're basically just accessing data via a URL (technically, an API)
+### but instead of doing through your browser you do it via Python.
+
+## here's an exma
+
